ee92994591dd67571714edadebb014643acb7a98
walkii <julien_denoulet@orange.fr>
2017-02-14 21:48:11 +0100

    déplacement automatique

--- src/lightBot/WindowsGame.java
+++ src/lightBot/WindowsGame.java
@@ -9,17 +9,22 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.*;
+import java.util.*;
 /**
  * @author ZMNF8866
  *
  */
-public class WindowsGame extends BasicGame {
 
+public class WindowsGame extends BasicGame {
+	
 	private GameContainer container;
 	private TiledMap map;
-	private float x = 55, y = 230;
+	private float x = 50, y = 50+(5*32)+(16);
 	private int direction = 3;
 	private boolean moving = false;
+	private String str="";
+	private boolean bool=false;
+	
 	private Animation[] animations = new Animation[8];
 	
 	public WindowsGame() {
@@ -34,6 +39,15 @@ public class WindowsGame extends BasicGame {
 		 g.setColor(new Color(0, 0, 0, .5f));
 		  g.fillOval(x - 16, y - 8, 32, 16);
 		   g.drawAnimation(animations[direction + (moving ? 4 : 0)], x-32, y-60);
+		   g.drawString( this.str , 0, 0);
+		   g.drawLine( 0, 400, 500, 400);
+		   g.drawLine( 500, 0, 500, 400);
+		   g.drawString( "controlleur" , 200, 500);
+		   g.drawString( "sequence" , 600, 200);
+		   g.setColor(new Color( 0,255 ,0 ));
+		   g.fillRect(400, 300, 100, 100);
+		   g.setColor(new Color( 0,0,0 ));
+		   g.drawString( "GO" , 440, 350);
 	}
 
 	@Override
@@ -55,28 +69,53 @@ public class WindowsGame extends BasicGame {
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
 //mise a jour des éléments
-		if (this.moving) {
+		str=""+delta;
+		/*if (this.moving) {
 	        switch (this.direction) {
-	            case 0: this.y -= .1f * delta; break;
-	            case 1: this.x -= .1f * delta; break;
-	            case 2: this.y += .1f * delta; break;
-	            case 3: this.x += .1f * delta; break;
+	            case 0: this.y -= 0.1f * delta; break; // mettre des limite pour ne pas sortir de la map
+	            case 1: this.x -= 0.1f * delta; break;
+	            case 2: this.y += 0.1f * delta; break;
+	            case 3: this.x += 0.1f * delta; break;
 	        }
-	    }
+	    }*/
 
+if(this.bool){
+if(this.x> 50+48 ){
+	this.direction=0;
+	if(this.y<(50+(5*32)+(16)-32)){
+		this.direction=3;
+		if(this.x>50+48+64){
+			this.direction=0;
+			if(this.y<(50+(5*32)+(16)-32)-96){
+				this.direction=3;
+				if(this.x>50+48+64+96){
+					this.moving=false;
+				}
+				else{
+					this.x += 0.1f * delta;
+				}
+			}
+			else{
+				this.y -= 0.1f * delta;
+			}
+		}
+		else{
+			this.x += 0.1f * delta;
+		}
 	}
-	
+	else{
+		this.y -= 0.1f * delta;
+	}	
+}
+else{
+	this.x += 0.1f * delta;
+}}
+
+}
 	public static void main(String[] args) throws SlickException {
         new AppGameContainer(new WindowsGame(), 680, 680, false).start();
     }
 
-	@Override
-	public void keyReleased(int key, char c) {
-	        if (Input.KEY_ESCAPE == key) {
-	            container.exit();
-	        }
-	        this.moving = false;
-	    }
 	
 	private Animation loadAnimation(SpriteSheet spriteSheet, int startX, int endX, int y) {
 	    Animation animation = new Animation();
@@ -91,8 +130,33 @@ public class WindowsGame extends BasicGame {
 	        case Input.KEY_UP:    this.direction = 0; this.moving = true; break;
 	        case Input.KEY_LEFT:  this.direction = 1; this.moving = true; break;
 	        case Input.KEY_DOWN:  this.direction = 2; this.moving = true; break;
-	        case Input.KEY_RIGHT: this.direction = 3; this.moving = true; break;
+	        case Input.KEY_RIGHT: this.direction = 3; this.moving = true; break;  
 	    }
 	}
+	
+	@Override
+	public void keyReleased(int key, char c) {
+	        if (Input.KEY_ESCAPE == key) {
+	            container.exit();
+	        }
+	        this.moving = false;
+	    }
+	
+	@Override
+	public void mousePressed(int button, int x, int y) {
+		switch (button) {
+		//case Input.isMouseButtonDown(button): 
+		case Input.MOUSE_LEFT_BUTTON:
+			if(400<x&& x<500 && 300<y && y<400) { this.x = 50; this.y = 50+(5*32)+(16); this.moving=true; this.bool=true;} 
+			else this.str="mouse active"; break;
+	}
+	}
+	@Override
+	public void mouseReleased(int button, int x, int y) {
+		 //this.str="";
+	}
+	
+	
+	}
 
-}
+//http://slick.ninjacave.com/javadoc/org/newdawn/slick/Input.html
